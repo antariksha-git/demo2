1f88c5cd03dc227f94c0e7fdb25b749fba091129
antariksha-git <heyantariksha@gmail.com>
2024-10-20 14:14:27 +0530

    Added userService

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repo.UserRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private UserRepo userRepo;
+
+    public User addUser(User user) {
+        return userRepo.save(user);
+    }
+
+}
